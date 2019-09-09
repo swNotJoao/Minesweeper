b914e5c20b1339ab051151e26279ef3996e1abd5
Joao <jclopes99@gmail.com>
2019-09-09 15:18:31 +0100

    Init commit

---
+++ src/tile.py
@@ -0,0 +1,20 @@
+class Tile:
+
+    def __init__(self, img, x, y, isMine):
+        self.img = img
+        self.x = x
+        self.y = y
+        self.state = 'closed'
+        self.isMine = isMine
+
+    def draw(self, window):
+        window.blit(self.img ,(self.x, self.y))
+
+    def open(self):
+        self.state = 'opened'
+
+    def setImg(self, img):
+        self.img = img
+
+    def getIsMine(self):
+        return isMine
