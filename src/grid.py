cbc3ba0b2e434da235f2b9a3140ff39d53bbe9ae
João <jclopes99@gmail.com>
2019-09-17 21:17:31 +0100

    grid completed, matrix fix pls

--- src/grid.py
+++ src/grid.py
@@ -1,30 +1,43 @@
 from tile import Tile
+import pygame
 
 class Grid:
-    def __init__(self, width ,height, bombNumber):
-        self.matrix = [[]]
+    def __init__(self, width ,height, bombNumber, window):
+        self.matrix = [[0]*10]*10
+        self.width = width
+        self.height = height
         self.bombNumber = bombNumber
 
         self.mineImg0 = pygame.image.load('../assets/0.png')
-        self.mineImg0 = pygame.transform.scale(mineImg0, (40, 40))
+        self.mineImg0 = pygame.transform.scale(self.mineImg0, (40, 40))
         self.mineImg1 = pygame.image.load('../assets/1.png')
-        self.mineImg1 = pygame.transform.scale(mineImg1, (40, 40))
+        self.mineImg1 = pygame.transform.scale(self.mineImg1, (40, 40))
         self.mineImg2 = pygame.image.load('../assets/2.png')
-        self.mineImg2 = pygame.transform.scale(mineImg2, (40, 40))
+        self.mineImg2 = pygame.transform.scale(self.mineImg2, (40, 40))
         self.mineImg3 = pygame.image.load('../assets/3.png')
-        self.mineImg3 = pygame.transform.scale(mineImg3, (40, 40))
+        self.mineImg3 = pygame.transform.scale(self.mineImg3, (40, 40))
         self.mineImg4 = pygame.image.load('../assets/4.png')
-        self.mineImg4 = pygame.transform.scale(mineImg4, (40, 40))
+        self.mineImg4 = pygame.transform.scale(self.mineImg4, (40, 40))
         self.mineImg5 = pygame.image.load('../assets/5.png')
-        self.mineImg5 = pygame.transform.scale(mineImg5, (40, 40))
+        self.mineImg5 = pygame.transform.scale(self.mineImg5, (40, 40))
         self.mineImg6 = pygame.image.load('../assets/6.png')
-        self.mineImg6 = pygame.transform.scale(mineImg6, (40, 40))
+        self.mineImg6 = pygame.transform.scale(self.mineImg6, (40, 40))
         self.mineImg7 = pygame.image.load('../assets/7.png')
-        self.mineImg7 = pygame.transform.scale(mineImg7, (40, 40))
+        self.mineImg7 = pygame.transform.scale(self.mineImg7, (40, 40))
         self.mineImg8 = pygame.image.load('../assets/8.png')
-        self.mineImg8 = pygame.transform.scale(mineImg8, (40, 40))
+        self.mineImg8 = pygame.transform.scale(self.mineImg8, (40, 40))
+        self.questionMarkImg = pygame.image.load('../assets/?.png')
+        self.questionMarkImg = pygame.transform.scale(self.questionMarkImg, (40, 40))
+        self.flagImg = pygame.image.load('../assets/flag.png')
+        self.flagImg = pygame.transform.scale(self.flagImg, (40, 40))
+        self.tileImg = pygame.image.load('../assets/tile.png')
+        self.tileImg = pygame.transform.scale(self.tileImg, (40, 40))
 
-    def init(self):
-        for x in range (0, 9):
-            for y in range (0, 9):
-                self.matrix[x][y] = Tile()
+        for x in range (0, self.width - 1):
+            for y in range (0, self.height - 1):
+                self.matrix[x][y] = Tile(self.tileImg, x*40, y*40, False)
+
+    def draw(self, window):
+        for x in range (0, self.width - 1):
+            for y in range (0, self.height - 1):
+                self.matrix[x][y].draw(window)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame, sys, os
 from tile import Tile
+from grid import Grid
 
 # Setup
 pygame.init()
@@ -7,20 +8,29 @@ pygame.display.set_caption('Minesweeper v1.0b')
 screen = pygame.display.set_mode((400, 400))
 programRunning = True
 
+grid = Grid(10, 10, 0, screen)
+
 # Main loop
 while programRunning:
+    #Check for quit event caused by user
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             programFinished = True
             pygame.quit()
             sys.exit()
 
+        #Draw frame!
+
+        grid.draw()
+
         if event.type == pygame.MOUSEBUTTONUP:
             # Set the x, y postions of the mouse click
             x, y = event.pos
             squarex, squarey = x//40, y//40
+            #Check for left mouse button press
             if event.button == 1:
                 print "[!] Touched", x, y, squarex, squarey
+            #Check for right mouse button press
             elif event.button == 3:
                 print "[!] Flagged", x, y, squarex, squarey
 
